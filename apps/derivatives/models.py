d5900c9a6020c9728935c25d2749b3e0afb149c1
evandro <evandro.b@hotmail.com>
2026-03-25 22:49:35 -0300

    após atualizações

--- apps/derivatives/models.py
+++ apps/derivatives/models.py
@@ -24,6 +24,7 @@ class DerivativeOperation(TenantAwareModel, CreatedByMixin, TimeStampedModel):
     ordem = models.PositiveIntegerField(default=1)
     posicao = models.CharField(max_length=20, blank=True, verbose_name="Posicao")
     safra = models.ForeignKey("clients.CropSeason", null=True, blank=True, on_delete=models.SET_NULL, related_name="derivativos")
+    swap_divida = models.CharField(max_length=10, blank=True, verbose_name="Se for Moeda Swap de Divida")
     strike_moeda_unidade = models.CharField(max_length=30, blank=True, verbose_name="Strike moeda unidade")
     status_operacao = models.CharField(max_length=30, blank=True, default="Em aberto")
     strike_liquidacao = models.DecimalField(max_digits=18, decimal_places=4, null=True, blank=True)
